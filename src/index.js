b8e5633905c2fe9f2047b653f0eebe8abd78aec6
Morgana Spake <morgana.spake@gmail.com>
2019-06-19 20:05:04 -0700

    message component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-// import './styles.css';
+
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Hello World',
+    };
+  }
+
+  render() {
+    return (<div> 
+      <p>{ this.state.text }</p> 
+      <img src="https://cdn140.picsart.com/262979961014212.png?r1024x1024"></img>
+      </div>);
+  }
+}
 
 class App extends React.Component {
   render() {
-    return <div />;
+    return <Message />;
   }
 }
 
